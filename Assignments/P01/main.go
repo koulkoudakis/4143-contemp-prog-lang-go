e71cbf743923883bfb73a6e13d1c60a73f70b428
Sharome Burton <58353987+koulkoudakis@users.noreply.github.com>
2023-09-12 10:44:39 -0500

    Add files via upload

---
+++ Assignments/P01/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/koulkoudakis/4143-contemp-prog-lang-go/mascot"
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(mascot.BestMascot())
+	fmt.Println(quote.Go())
+}
