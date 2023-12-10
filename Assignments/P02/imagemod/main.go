7f48a30abbfa5b93f5ac53c0a5a549ab60b9896a
KOULKOUDAKIS\Koulk <koulkoudakis@gmail.com>
2023-12-09 19:55:06 -0600

    P02

---
+++ Assignments/P02/imagemod/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"myimageapp/imagemod"
+)
+
+func main() {
+	// Create an ImageManipulator instance with an existing image
+	im, err := imagemod.NewImageManipulatorWithImage("mustangs.jpg")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Draw a rectangle
+	im.DrawRectangle(150, 50, 560, 411)
+
+	// Save the modified image to a file
+	im.SaveToFile("mustangs_edit.png")
+}
