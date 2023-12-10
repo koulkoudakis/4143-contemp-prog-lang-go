b24099485fe6ea99aedc447e5648f9bd151ee897
KOULKOUDAKIS\Koulk <koulkoudakis@gmail.com>
2023-12-10 10:45:35 -0600

    P03

--- Assignments/P03/main.go
+++ Assignments/P03/main.go
@@ -4,35 +4,21 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/koulkoudakis/img_mod/Colors"
-	"github.com/koulkoudakis/img_mod/Grayscale"
-	"github.com/koulkoudakis/img_mod/Text"
-	"github.com/koulkoudakis/img_mod/getPic"
 )
 
 func main() {
-	// Prints Heading of the Program
 	fmt.Println("Sharome Burton")
 	fmt.Println("Program 3 - Image Ascii Art")
 
-	fmt.Print("\n")
+	// get picture from URL
+	getPic.main()
 
-	// Call function to get picture from URL
-	getPic.DownloadPicture()
+	// process pixel colors
+	Colors.main()
 
-	fmt.Print("\n")
+	// grayscale image
+	Grayscale.main()
 
-	// Call function to process Pixel Colors
-	Colors.PrintPixels()
-
-	fmt.Print("\n")
-
-	// Call function to grayscale image
-	Grayscale.GrayScale()
-
-	fmt.Print("\n")
-
-	// Call function to print colored text to image
-	Text.PrintText()
+	// print colored text to image
+	Text.main()
 }
